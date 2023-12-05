24dab551bb71241e9448b341d3f5c79827323614
Yousaf112 <yyousafahmedd@gmail.com>
2023-12-05 10:29:58 +0500

    Done

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import Nav from "./nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+import Button from "./button";
+
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
