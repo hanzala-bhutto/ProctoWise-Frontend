84b3d5a8f637c6892b36a209dc8825ef5536fb23
Yousaf112 <yyousafahmedd@gmail.com>
2023-12-14 11:14:51 +0500

    @Fix | Hierarchy of Directory

---
+++ src/app/participant/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from "../participantComponents/NavigationBar";
+
+const page = () => {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/participant/participantComponents/NavigationBar.tsx
@@ -0,0 +1,5 @@
+const NavigationBar = () => {
+  return <h1>Navbar</h1>;
+};
+
+export default NavigationBar;
