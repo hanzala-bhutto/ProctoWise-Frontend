7eb7f2f9bf5284e817bc363cffe63042d56412d4
hanzala-bhutto <bhutto.hanzala@gmail.com>
2024-03-04 19:41:51 +0500

    view all events integration

--- src/app/participant/participantComponents/EventCard.tsx
+++ src/app/participant/participantComponents/EventCard.tsx
@@ -16,33 +16,34 @@ interface EventProps {
   id: number;
   title: string;
   description: string;
-  content: string;
+  // content: string;
 }
-const propTypes = {
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-};
+// const propTypes = {
+//   id: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   description: PropTypes.string.isRequired,
+//   content: PropTypes.string.isRequired,
+// };
 
 const EventCard: React.FC<EventProps> = ({
   id,
   title,
   description,
-  content,
+  // content,
 }) => {
   return (
-    <Card className=" w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 my-2 mx-2 bg-blue-900 text-white">
+    <Card className="flex flex-col w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 my-2 mx-2 bg-blue-900 text-white">
       <CardHeader>
         <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
+        <CardDescription className="line-clamp-2">{description}</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>{content}</p>
+        {/* <p>{content}</p> */}
+        <p>id: {id}</p>
       </CardContent>
-      <CardFooter>
+      <CardFooter className="mt-auto">
         <p className="flex justify-center ">
-          <PaymentToggler />
+          <PaymentToggler id={id}/>
           <Button asChild>
             <Link href={`/participant/joinevent/${encodeURIComponent(id)}`}>
               Info <InfoIcon className="ml-2" />
@@ -53,6 +54,6 @@ const EventCard: React.FC<EventProps> = ({
     </Card>
   );
 };
-EventCard.propTypes = propTypes;
+// EventCard.propTypes = propTypes;
 
 export default EventCard;
